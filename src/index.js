8e29be7ea5b474b2847a76501e052a22a188b3f4
C0deNe0 <access.naveenhiremath@gmail.com>
2025-05-16 22:57:51 +0530

    initial setup

---
+++ src/index.js
@@ -0,0 +1,40 @@
+const serverless = require("serverless-http");
+const express = require("express");
+const { neon,neonConfig } = require("@neondatabase/serverless");
+
+
+const app = express();
+
+async function dbClient() {
+  const sql = neon(process.env.DATABASE_URL)
+  return sql
+}
+
+app.get("/",async (req, res, next) => {
+
+  //for http- connections
+  //non polling
+  neonConfig.fetchConnectionCache = true
+
+  const sql =await dbClient()
+  const [result] = await sql`select now()`  //actually giving a result array
+
+  return res.status(200).json({
+    message: "Hello from root!",
+    result: result.now,  
+  });
+});
+
+app.get("/hello", (req, res, next) => {
+  return res.status(200).json({
+    message: "Hello from path!",
+  });
+});
+
+app.use((req, res, next) => {
+  return res.status(404).json({
+    error: "Not Found",
+  });
+});
+
+exports.handler = serverless(app);
